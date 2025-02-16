cb9adcf0de8ae7c2aaaf7993015c6c6645d48b72
Cyber Phantom <inam123451@gmail.com>
2025-02-17 01:34:20 +0530

    Add .gitignore

--- target/debug/deps/atk_command_derive-da5952fbb4665999.d
+++
@@ -1,5 +0,0 @@
-/home/phantom/Projects/atk_stuff/atk-command-derive/target/debug/deps/libatk_command_derive-da5952fbb4665999.rmeta: src/lib.rs
-
-/home/phantom/Projects/atk_stuff/atk-command-derive/target/debug/deps/atk_command_derive-da5952fbb4665999.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/atk_command_derive-f65bb857f6030a31.d
+++
@@ -1,5 +0,0 @@
-/home/phantom/Projects/atk_stuff/atk-command-derive/target/debug/deps/libatk_command_derive-f65bb857f6030a31.rmeta: src/lib.rs
-
-/home/phantom/Projects/atk_stuff/atk-command-derive/target/debug/deps/atk_command_derive-f65bb857f6030a31.d: src/lib.rs
-
-src/lib.rs:
